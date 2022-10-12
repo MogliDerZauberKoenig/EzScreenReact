e1be6ae9b117467c501d39bded74b2516dd828ab
MogliDerZauberKoenig <niko_faifert@web.de>
2022-10-13 00:32:34 +0200

    Version 0.1.3

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -55,7 +55,7 @@ class App extends Component {
           <Route path="/" element={<Hello />} />
           <Route path="/Settings" element={<Settings />} />
         </Routes>
-        <p>Version: 0.1.2</p>
+        <p>Version: 0.1.3</p>
       </Router>
     );
   }
